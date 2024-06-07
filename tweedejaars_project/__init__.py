b3fb08a41d92e4eeba1cb6f642d551007443ddba
Steven Dong <sdong488@gmail.com>
2024-06-07 11:40:27 +0200

    load save model

--- tweedejaars_project/__init__.py
+++ tweedejaars_project/__init__.py
@@ -1,3 +1,3 @@
 from . import config  # noqa: F401
-from .data import load_df, save_df  # noqa: F401
+from .data import load_df, save_df, load_model, save_model  # noqa: F401
 from .data import get_splits  # noqa: F401
--- tweedejaars_project/data/fileloader.py
+++ tweedejaars_project/data/fileloader.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import pandas as pd
+import pickle
 
-from ..config import PROCESSED_DATA_DIR, MAIN_DATA_FILE_NAME
+from ..config import PROCESSED_DATA_DIR, MAIN_DATA_FILE_NAME, MODELS_DIR
 
 
 def load_df(file_path: Path = PROCESSED_DATA_DIR / MAIN_DATA_FILE_NAME):
@@ -16,3 +17,15 @@ def save_df(
 ):
     """Save the dataframe to a pickle file."""
     pd.to_pickle(df, file_path)
+
+
+def load_model(file_name: str):
+    """Load a model from a pickle file."""
+    file_path = MODELS_DIR / file_name
+    return pickle.load(file_path)
+
+
+def save_model(file_name: str):
+    """Save a model to a pickle file."""
+    file_path = MODELS_DIR / file_name
+    pickle.dump(file_path)
