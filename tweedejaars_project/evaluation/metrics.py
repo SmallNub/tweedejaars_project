e70f3850c2eb5267c6c94d472fa26281bee836bc
Steven Dong <sdong488@gmail.com>
2024-06-09 19:50:38 +0200

    .

--- tweedejaars_project/evaluation/metrics.py
+++ tweedejaars_project/evaluation/metrics.py
@@ -74,11 +74,11 @@ def show_real_penalty_score(df: pd.DataFrame, true: pd.Series, pred: pd.Series, 
     false_pos_penalty_total_sum = false_pos_penalty_total.sum()
 
     # Invert it so it makes more logical sense
-    false_neg_penalty_sum = false_neg_penalty_total_sum - false_neg_penalty_sum
-    false_pos_penalty_sum = false_pos_penalty_total_sum - false_pos_penalty_sum
+    # false_neg_penalty_sum = false_neg_penalty_total_sum - false_neg_penalty_sum
+    # false_pos_penalty_sum = false_pos_penalty_total_sum - false_pos_penalty_sum
 
-    print(f"False negative score (pred/max): {false_neg_penalty_sum}/{false_neg_penalty_total_sum}")
-    print(f"False positive score (pred/max): {false_pos_penalty_sum}/{false_pos_penalty_total_sum}")
+    print(f"False negative score (pred/max): {false_neg_penalty_sum / false_neg_penalty_total_sum}, {false_neg_penalty_sum}/{false_neg_penalty_total_sum}")
+    print(f"False positive score (pred/max): {false_neg_penalty_sum / false_neg_penalty_total_sum}, {false_pos_penalty_sum}/{false_pos_penalty_total_sum}")
     # return false_neg_penalty_sum, false_neg_penalty_total_sum, false_pos_penalty_sum, false_pos_penalty_total_sum
 
 # TODO make it work better with adjustment
