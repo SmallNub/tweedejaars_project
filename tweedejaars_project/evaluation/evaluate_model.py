aec1628daceb1d6fa12b51261f87d29a4bfeb359
Steven Dong <sdong488@gmail.com>
2024-06-22 00:23:03 +0200

    test

--- tweedejaars_project/evaluation/evaluate_model.py
+++ tweedejaars_project/evaluation/evaluate_model.py
@@ -21,8 +21,8 @@ def evaluate_income(
         # Train
         model = train_func(train_in, train_out)
         # Test and adjust predictions
-        pred = test_func(model, test_in)
-        pred = adjust_pred_realtime(realtime, pred)
+        pred_adj, pred = test_func(model, test_in)
+        pred_adj = adjust_pred_realtime(realtime, pred_adj)
 
         # Make consistent first (then conform (not relevant for income))
         # pred_con = adjust_pred_consistency(pred)
@@ -32,9 +32,9 @@ def evaluate_income(
         # pred_con2 = adjust_pred_consistency(pred)
 
         # Compute scores
-        preds = [pred]
+        preds = [pred_adj]
         scores = [compute_income_score_added(action, price, p.to_numpy(copy=True), naive) for p in preds]
-        return model, scores
+        return model, scores, pred
 
     # Predefined
     realtime = test_df[f"{version}_two_sided_ptu_realtime"].to_numpy(copy=True)
@@ -44,19 +44,29 @@ def evaluate_income(
 
     results = Parallel(n_jobs=-1)(delayed(step)(train_in, train_out, test_in, realtime, action, price, naive) for _ in range(repeat))
 
-    n_out = 1
-    out = [[None, -np.inf, []] for _ in range(n_out)]
-    for model, scores in results:
+    # Number of outputs
+    n_out = len(results[0][1])
+
+    # Outputs (model, best score, score x repeat, best base prediction) x n_out
+    out = [[None, -np.inf, [], None] for _ in range(n_out)]
+
+    # Iterate every result
+    for model, scores, preds in results:
+        # Iterate every score
         for i, score in enumerate(scores):
-            out[i][2].append(score)
+            out[i][2].append(score)  # Save the score for every output
+            # Save the best score and the corresponding model for the corresponding output type
             if score > out[i][1]:
                 out[i][0] = model
                 out[i][1] = score
+                out[i][3] = preds
 
-    best = [None, -np.inf]
+    # Get the overall best
+    best = [None, -np.inf, None]
     for i in range(n_out):
         if out[i][1] > best[1]:
-            best = out[i][:2]
+            best[:2] = out[i][:2]
+            best[2] = out[i][3]
 
     for i in range(n_out):
         out[i][2] = pd.Series(out[i][2], name=str(i))
